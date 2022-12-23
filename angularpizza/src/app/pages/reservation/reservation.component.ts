3ee7af4e363473173363cbc1b51a70a3719b1ded
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-12-23 13:42:45 -0300

    All forms configurated

--- angularpizza/src/app/pages/reservation/reservation.component.ts
+++ angularpizza/src/app/pages/reservation/reservation.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TranslateService } from "@ngx-translate/core";
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-reservation',
@@ -12,12 +14,29 @@ export class ReservationComponent implements OnInit {
   logoPath!: string
   restaurantPath!: string
 
-  constructor(private translate: TranslateService) {
+  reservationForm!: FormGroup
+
+  constructor(private translate: TranslateService, private fb: FormBuilder, private userService: UserService) {
     this.translate.setDefaultLang('pt-bt');
     this.translate.use('pt-br');
 
     this.logoPath = '/assets/img/logo.svg'
     this.restaurantPath = '/assets/img/entrance.jpg'
+
+    this.reservationForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      date: ['', Validators.required],
+      tableFor: ['', Validators.required]
+    })
+  }
+
+  handleReservation() {
+    alert(`Parabéns! Sua reserva foi realizada com sucesso, Dados da reserva:
+      Está no nome de: ${this.reservationForm.value.name} ;
+      Mesa para: ${this.reservationForm.value.tableFor} ;
+      Data: ${this.reservationForm.value.date}
+    `)
   }
 
   ngOnInit(): void {
--- angularpizza/src/app/pages/sign-in/sign-in.component.ts
+++ angularpizza/src/app/pages/sign-in/sign-in.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TranslateService } from "@ngx-translate/core";
+import { UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -11,14 +13,30 @@ import { TranslateService } from "@ngx-translate/core";
 export class SignInComponent implements OnInit {
   logoPath!: string
 
-  constructor(private translate: TranslateService) {
+  signInForm!: FormGroup
+
+  constructor(
+    private translate: TranslateService,
+    private fb: FormBuilder,
+    private userService: UserService
+    ) {
     this.translate.setDefaultLang('pt-bt');
     this.translate.use('pt-br');
 
     this.logoPath = '/assets/img/logo.svg'
+
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.minLength(8)]],
+      password:  ['', [Validators.required]]
+    }
+    )
    }
 
   ngOnInit(): void {
+  }
+  
+  handleSignIn(){
+    console.log(this.signInForm.value)
   }
 
   changeLang(event: any) {
